d21d3c2141f2a7e82e06271031b33d04e78eab50
wafazitouna <wafa.zitouna@bsamply.com>
2020-01-28 15:59:15 +0100

    Define general SVG component

--- src/app/shared/components/shared.module.ts
+++ src/app/shared/components/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { SvgIconComponent } from './svg-icon/svg-icon.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent,FooterComponent],
-  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent,FooterComponent, HeaderMenuComponent, SvgIconComponent],
+  exports: [HeaderComponent, FooterComponent, HeaderMenuComponent],
   imports: [
     CommonModule
   ]
