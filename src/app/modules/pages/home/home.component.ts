cc8ebe8988f435a34096da37fa9690875e48ce24
wafazitouna <wafa.zitouna@bsamply.com>
2020-01-29 16:52:51 +0100

    WIP:Home page

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { products } from './../../../data/products';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  productList = products;
   constructor() { }
 
   ngOnInit() {
