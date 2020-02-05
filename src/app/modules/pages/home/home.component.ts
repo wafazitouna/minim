3459a69d39288bdf61c4edebc46a2a3bd798744e
wafazitouna <wafa.zitouna@bsamply.com>
2020-02-05 17:38:19 +0100

    get product in home page from service

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from './../../../data/products';
+import { PaginatorService } from 'src/app/shared/services/paginator.service';
+import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,48 @@ import { products } from './../../../data/products';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  productList = products;
-  constructor() { }
 
-  ngOnInit() { }
+  // array of all items to be paged
+  private productList: any[];
+
+  // pager object
+  pager: any = {};
+
+  // paged items
+  pagedProducts: any[];
+
+  constructor(private productService: ProductService, private paginatorService: PaginatorService) { }
+
+  ngOnInit() {
+    this.productService.getAll().subscribe((data) => {
+      // set items to json response
+      this.productList = data;
+      // initialize to page 1
+      this.setPage(1);
+
+    })
+  }
+
+
+  setPage(page: number) {
+
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+
+    // get pager object from service
+    this.pager = this.paginatorService.getPager(this.productList.length, page, 8);
+
+    // get current page of products
+    this.pagedProducts = this.productList.slice(this.pager.startIndex, this.pager.endIndex + 1);
+
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+
 
 }
